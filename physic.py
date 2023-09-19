d2bb5ab638471c12b2aa2d9d47f95b3755079f1b
DenMagiskeMannen <teodor@solbu.org>
2023-09-19 20:28:39 +0200

    Add files via upload

---
+++ physic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 13:52:58 2023
+
+@author: teodo
+"""
+
+import random
+import matplotlib.pyplot as plt
+
+startUp=0
+startRight=0
+
+startX=0
+startY=10
+
+g=9.81
+m=10
+G=m*g
+
+Xses=[]
+for i in range(100):
+    Xses.append(i)
+
+Yses=[]
+y=startY
+while True:
+    y=y-
+    
+
+
