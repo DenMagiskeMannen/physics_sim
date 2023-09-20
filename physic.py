ad200a16cf9eda56e0fd402753b942d9829eefbf
DenMagiskeMannen <teodor@solbu.org>
2023-09-19 23:11:00 -0700

    Update physic.py
    inscribes plot function

--- physic.py
+++ physic.py
@@ -8,9 +8,26 @@ import time
 import random
 import matplotlib.pyplot as plt
 
+
+
+def limfind(list):
+    smallest=0
+    biggest=0
+    for number in list:
+        if number < smallest:
+            smallest=number
+        elif number > biggest:
+            biggest= number
+    return(smallest,biggest)
+
+def establish_para():
+    plt.xlim(xlim0,xlim1)
+    plt.ylim(ylim0,ylim1+10)
+
+
 #Vo
 startUp=5
-startRight=0
+startRight=5
 
 #So
 startX=0
@@ -21,7 +38,7 @@ g=-9.81
 m=10
 G=m*g
 
-Xconstant=0
+Xresistence=0
 
 #T
 timestep=0.01
@@ -41,13 +58,15 @@ Xvector=startRight
 Distances=[]
 Heights=[]
 
+
+
 for frame in times:
-    print(frame,currentY)
+    #print(frame,currentY)
     #Ratio
     ratio=1/timestep
     
     #MovementX
-    Xvector=Xvector+(Xconstant/ratio)
+    Xvector=Xvector+(Xresistence/ratio)
     currentX=currentX+(Xvector)
     #Movement Y
     Yvector=Yvector+(g/ratio)
@@ -59,21 +78,41 @@ for frame in times:
     
     Distances.append(currentX)
     Heights.append(currentY)
+
+
+xlim0,xlim1 =limfind(Distances)
+if xlim0 == xlim1:
+    xlim0-=1
+    xlim1+=1
+ylim0,ylim1 =limfind(Heights)
+if ylim0 == ylim1:
+    ylim0-=1
+    ylim1+=1
+#print(xlim0,xlim1,"\n",ylim0,ylim1)
+
+dist=5
+for i in times:
+    #print(i,Distances[i],Heights[i])
     
     
+    establish_para()
     
-    """
-    #Every SECOND
-    print(currentX,currentY)
-    #MovementX
-    Xvector=Xvector+Xconstant
-    currentX=currentX+(Xvector)
-    #Movement Y
-    Yvector=Yvector+g
-    currentY=currentY+Yvector
-    """
-    #time.sleep(1)
-    
-    
-plt.plot(times,Heights)
+    if i % dist == 0:
+        plt.scatter(Distances[i],Heights[i])
+        plt.plot(Distances[:i],Heights[:i])
+        plt.show()
+        time.sleep(0.1)
+
+"""
+if frame % fta ==0:
+    plt.scatter(Distances[-1],Heights[-1])
+    plt.show()
+    time.sleep(0.5)
+"""
+establish_para()
+plt.plot(Distances,Heights)
+#plt.subplot(2,1,1)
+#plt.plot(times,Heights)
+
+
 
